4fd6115d348eb1fd2c2b4386b5d2ae113f775fce
Amie Shanaya Wijeratne <72076192+22012825@users.noreply.github.com>
2022-03-11 09:29:17 +0000

    Add project files.

---
+++ StudentGrades.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using ConsoleAppProject.Helpers;
+
+namespace ConsoleAppProject.App03
+{
+    /// <summary>
+    /// At the moment this class just tests the
+    /// Grades enumeration names and descriptions
+    /// </summary>
+    public class StudentGrades
+    {
+        private readonly StudentGrades
+            converter = new StudentGrades();
+
+        private readonly int[] StatsMarks = new int[]
+        {
+            10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+        };
+
+        private int[] testMarks;
+
+        [TestMethod]
+        public void TestConvert0ToGradeF { }
+        {
+            //Arrange
+            StudentGrades expectedGrade = Grades.F;
+            
+            //Act
+            StudentGrades actualGrade
+
+    }
+}
